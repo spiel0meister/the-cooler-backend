76efb22b279b2d4fe7f78ea4e5ffc36e57b2d9e9
Žan Sovič <soviczan7@gmail.com>
2025-12-16 18:33:53 +0100

    basic backend

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import { createServer } from "node:http";
 import process from "node:process";
 import express from "express";
 import session from "express-session";
-import { MongoClient } from "mongodb";
+import mongoose, { Schema, model, connect } from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -15,14 +16,16 @@ const server = createServer(app);
 const mongo_uri = process.env.MONGO_URI;
 
 if (mongo_uri == undefined) throw "No mongo URI";
-const client = new MongoClient(mongo_uri);
+connect(mongo_uri);
 
-let conn;
-try {
-    conn = await client.connect();
-} catch (e) {
-    throw e;
-}
+const UserSchema = new Schema({
+    name: String,
+    surname: String,
+    email: String,
+    password: String,
+});
+
+const User = model("User", UserSchema);
 
 app.use(session({
     secret: "very-secret-secret",
@@ -30,18 +33,82 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false },
 }));
+app.use(cors());
 
 app.get("/api/logout", (req, res) => {
-    res.send("TODO");
+    if (req.session.email == null) {
+        res.sendStatus(400);
+        return;
+    }
+
+    res.session.email = null;
+    res.sendStatus(200);
 });
 
-app.get("/api/login", (req, res) => {
-    res.send("TODO");
+app.get("/api/login", async (req, res) => {
+    const email = req.query.email;
+    if (email == null) {
+        console.error("No email");
+        res.sendStatus(400);
+        return;
+    }
+
+    const password = req.query.password;
+    if (password == null) {
+        console.error("No password");
+        res.sendStatus(400);
+        return;
+    }
+
+    const user = await User.findOne({email, password});
+    if (!user) {
+        res.sendStatus(400);
+        return;
+    }
+
+    res.session.email = user.email;
+    res.sendStatus(200);
 });
 
-app.get("/api/signup", (req, res) => {
-    console.log(req);
-    res.send("TODO");
+app.get("/api/signup", async (req, res) => {
+    const name = req.query.name;
+    if (name == null) {
+        console.error("No name");
+        res.sendStatus(400);
+        return;
+    }
+
+    const surname = req.query.surname;
+    if (surname == null) {
+        console.error("No surname");
+        res.sendStatus(400);
+        return;
+    }
+
+    const email = req.query.email;
+    if (email == null) {
+        console.error("No email");
+        res.sendStatus(400);
+        return;
+    }
+
+    const password = req.query.password;
+    if (password == null) {
+        console.error("No password");
+        res.sendStatus(400);
+        return;
+    }
+
+    const user = new User({ name, surname, email, password });
+
+    const userAlreadyExits = await User.findOne({ email }).exec();
+    if (userAlreadyExits) {
+        console.error("Already exists");
+        res.sendStatus(400);
+        return;
+    }
+
+    await user.save();
 });
 
 server.listen(port, () => {
